d57c513cb3970f3af63e75ff4d5b1742c1b4325c
Geeta <134486497+GeetaGVM@users.noreply.github.com>
2024-01-11 19:49:24 +0530

    done deploying

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const qrRoutes = require('./routes/qrRoutes');
 const path = require('path');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json'); 
-const { dashLogger } = require("./utils/logger");
+// const { dashLogger } = require("./utils/logger");
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 app.use(bodyParser.json());
 
@@ -31,12 +31,12 @@ app.use((req, res, next) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  dashLogger.error(`${err}, \nRequest: ${req.originalUrl}, \nRequest Params: ${JSON.stringify(req.query)}, \nRequest Body: ${JSON.stringify(req.body)}`);
-  res.status(500).json({ message: 'Something went wrong' });
-});
+// // Error handling middleware
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   dashLogger.error(`${err}, \nRequest: ${req.originalUrl}, \nRequest Params: ${JSON.stringify(req.query)}, \nRequest Body: ${JSON.stringify(req.body)}`);
+//   res.status(500).json({ message: 'Something went wrong' });
+// });
 
 // Start the server
 app.listen(PORT, () => {
